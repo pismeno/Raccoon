5e99a953a006358d21e4556990b802a31bdb4af9
pismeno <krystof.kloz@gmail.com>
2026-03-20 15:57:26 +0100

    print, compile commands

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -1,4 +1,3 @@
-#include <llvm/IR/Module.h>
 #include <iostream>
 #include <memory>
 #include "include/Lexer.hpp"
@@ -27,23 +26,20 @@ int main(int argc, char** argv) {
     CLI::App raccoonCli{"Raccoon Compiler"};
 
     CLI::App* print = raccoonCli.add_subcommand("print", "Compile a source file and output the IR in the console.");
+    CLI::App* compile = raccoonCli.add_subcommand("compile", "Compile a source file and write it inside build/");
 
     std::string sourceFileName;
     std::string outputFileName = "output.ll";
 
     print->add_option("source", sourceFileName, "Source file to compile")->required();
-    print->add_option("-o,--output", outputFileName, "Output binary name");
+    compile->add_option("source", sourceFileName, "Source file to compile")->required();
+    compile->add_option("-o,--output", outputFileName, "Output binary name");
 
     CLI11_PARSE(raccoonCli, argc, argv);
 
-    if (*print) {
+    if (*print || *compile) {
         Lexer lexer(get_file_contents(sourceFileName));
         auto tokens = lexer.tokenize();
-
-        for (const auto& token : tokens) {
-            std::cout << "Token Type: " << static_cast<int>(token.type)
-                      << " | Lexeme: [" << token.lexeme << "]" << std::endl;
-        }
 
         Parser parser(tokens);
 
@@ -51,32 +47,42 @@ int main(int argc, char** argv) {
             std::unique_ptr<ast::BlockStmt> root = parser.parse();
 
             ast::SemanticAnalyzer semanticAnalyzer;
-            ast::Printer printer;
             ast::IRGenerator irGenerator;
 
             root->accept(semanticAnalyzer);
-
-            std::cout << "--- Raccoon AST ---" << std::endl;
-            root->accept(printer);
-
-            std::cout << "--- Raccoon IR ---" << std::endl;
             root->accept(irGenerator);
-            irGenerator.module->print(llvm::outs(), nullptr);
 
-            std::ofstream file(outputFileName);
-            if (!file.is_open()) {
-                std::cerr << "Could not open file '" << outputFileName << "' for writing." << std::endl;
-                return 1;
+            if (*print) {
+                std::cout << "--- Raccoon Tokens ---" << std::endl;
+                for (const auto& token : tokens) {
+                    std::cout << "Token Type: " << static_cast<int>(token.type)
+                              << " | Lexeme: [" << token.lexeme << "]" << std::endl;
+                }
+
+                std::cout << "--- Raccoon AST ---" << std::endl;
+                ast::Printer printer;
+                root->accept(printer);
+
+                std::cout << "--- Raccoon IR ---" << std::endl;
+                irGenerator.module->print(llvm::outs(), nullptr);
             }
 
-            llvm::raw_os_ostream dest(file);
+            if (*compile) {
+                std::filesystem::create_directories("build");
+                std::ofstream file("build/" + outputFileName);
+                if (!file.is_open()) {
+                    std::cerr << "Could not open file '" << outputFileName << "' for writing." << std::endl;
+                    return 1;
+                }
 
-            irGenerator.module->print(dest, nullptr);
-            dest.flush();
-            file.close();
+                llvm::raw_os_ostream dest(file);
 
-            std::cout << "Successfully generated output.ll!" << std::endl;
+                irGenerator.module->print(dest, nullptr);
+                dest.flush();
+                file.close();
 
+                std::cout << "Successfully generated output.ll!" << std::endl;
+            }
         } catch (const ParseError& e) {
             std::cerr << e.what() << std::endl;
         }
